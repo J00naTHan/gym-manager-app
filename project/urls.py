8be57cfc0fc1bcf83d11fb12c0316b7e194f8882
J00naTHan <jonathan.dutra.2004@gmail.com>
2024-11-25 18:00:56 -0300

    user's mapping

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from users.views import ClientViewSet
+
+router = DefaultRouter()
+router.register(r'clientes', ClientViewSet, basename='cliente')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
